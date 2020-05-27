93c0c9ed5777c1cbb5c0bdb603bc633cd6cc6931
Leigh Halliday <leighhalliday@gmail.com>
2020-05-26 21:51:19 -0400

    Adding title

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import "../styles.css";
+import Head from "next/head";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>usePairs()</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
